b68bd93653446e113583da5144a93ff06f3278a8
Ben Flood <B00145093@mytudublin.ie>
2023-11-08 10:26:57 +0000

    completed part 1

---
+++ Team.php
@@ -0,0 +1,31 @@
+<?php
+class Team
+{
+	private $name="";
+	private $totalGames=0;
+	private $totalPoints=0;
+	private $totalGoals=0;
+	
+	public function __construct($parm1)
+	{
+		$this->name=$name;
+		
+	}
+	
+	public function finalScore($homeScore, $awayScore)
+	{
+		$this->totalGames++;
+		$this->totalGoals += $homeTeamScore;
+		
+		if ($homeTeamScore > $opposingTeamScore) {
+            $this->totalPoints += 3;
+        } elseif ($homeTeamScore == $opposingTeamScore) {
+            $this->totalPoints += 1;
+        }
+
+
+    }
+}
+?>
+	
+	
